73244b1e442a74d56dc2d8b251ade717fbdd310b
arunahugar <arunahugar348@gmail.com>
2020-12-04 20:18:12 +0530

    text

---
+++ Greater.java
@@ -0,0 +1,14 @@
+public class  Greater{
+publi satic void main(String args[]){
+Scanner sc =new Scanner(System.in);
+System.out.println("Enter first number");
+int first=sc.nextInt();
+System.out.println("Enter seond number");
+int second=sc.nextInt();
+if(first>second){
+System.out.println(first+"is greater");
+}
+else{
+System.out.println(seond+"is greater");
+}
+}
